804635e74377798894cd096e86dd30a747c9c337
Matheus Lins <msl@cin.ufpe.br>
2016-10-29 00:41:55 -0300

    crate models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from autoslug.fields import AutoSlugField
 
-# Create your models here.
+class Box(models.Model):
+	name = models.CharField('Nome', max_length=100)
+	slug = AutoSlugField("Slug",populate_from='name',unique=True)
+	create_date = models.DateField('Creation Date', auto_now_add=True)
+
+	class Meta:
+		verbose_name = 'Box'
+		verbose_name_plural = 'Boxes'
+
+	def __init__(self, arg):
+		super(Box, self).__init__()
+		self.name = arg
+		
+
+class BoxLog(models.Model):
+	box = models.ForeignKey(Box, verbose_name="Box Log", related_name='log_box')
+	datetime = models.DateTimeField("Date and Time of action", auto_now_add=True)
+
+	class Meta:
+		verbose_name = 'Box Log'
+		verbose_name_plural = 'Boxes Logs'
