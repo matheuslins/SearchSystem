ba9e3a98490c917993acc0753ea54fe93792e46a
Matheus Lins <msl@cin.ufpe.br>
2016-10-29 01:24:32 -0300

    search bar view and template

--- core/views.py
+++ core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Box
+import operator
+from django.db.models import Q
+from functools import reduce
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 class IndexView(generic.ListView):
@@ -8,6 +11,18 @@ class IndexView(generic.ListView):
 	queryset = Box.objects.all()
 	context_object_name = 'boxes'
 	paginate_by = 4
+
+	def get_queryset(self):
+
+		result = super(IndexView, self).get_queryset()
+		box_search = self.request.GET.get('search', None)
+		if box_search:
+			query_list = box_search.split()
+			result = result.filter(
+			reduce(operator.and_,
+				(Q(name__icontains=search) for search in query_list))
+			)
+		return result
 
 	def get_context_data(self, **kwargs):
 		context = super(IndexView, self).get_context_data(**kwargs)
