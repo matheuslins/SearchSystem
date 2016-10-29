3a4f1acd339b6507f5b75449650a6ff89fc11041
Matheus Lins <msl@cin.ufpe.br>
2016-10-28 23:05:09 -0300

    first commit - project base

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+urlpatterns = [
+	url(r'^$', views.IndexView.as_view(), name='core'),
+]
