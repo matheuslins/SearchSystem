ce010e7efe2d18d3b665e317c9d8e00f2357b26f
Matheus Lins <msl@cin.ufpe.br>
2016-10-29 00:14:15 -0300

    base template

--- searchsystem/settings.py
+++ searchsystem/settings.py
@@ -128,3 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "searchsystem/static")
+]
+
+try:
+    from searchsystem.dev import *
+except ImportError:
+    pass
