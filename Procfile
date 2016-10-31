5567d0a808d22f67571d975c46eef2094c506f47
Matheus Lins <msl@cin.ufpe.br>
2016-10-31 06:31:14 -0300

    ...

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn searchsystem.wsgi --log-file -
--- searchsystem/wsgi.py
+++ searchsystem/wsgi.py
@@ -8,12 +8,11 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'searchsystem.settings.common'
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "searchsystem.settings")
-
-from whitenoise.django import DjangoWhiteNoise
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
