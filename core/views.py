aae715e574896616ccea36f6f725de22ccd9475d
Matheus Lins <msl@cin.ufpe.br>
2016-10-31 05:22:19 -0300

    order_by datetime

--- core/views.py
+++ core/views.py
@@ -134,7 +134,7 @@ class LogView(generic.ListView):
 		except EmptyPage:
 		    list_box_log = paginator.page(paginator.num_pages)
 
-		context['list_box_log'] = list_box_log
+		context['list_box_log'] = list_box_log.order_by('datetime')
 
 		return context
 
